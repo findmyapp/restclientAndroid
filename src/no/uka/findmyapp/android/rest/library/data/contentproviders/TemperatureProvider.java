1cb83cf61ea8165e08fdcbecce417029cba0a22f
Torstein Melhus Barkve <torstein@barkve.no>
2011-07-08 00:10:05 +0200

    Added data-files

---
+++ src/no/uka/findmyapp/android/rest/library/data/contentproviders/TemperatureProvider.java
@@ -0,0 +1,145 @@
+package no.uka.findmyapp.android.rest.library.data.contentproviders;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import no.uka.findmyapp.android.rest.library.data.helpers.TemperatureDbHelper;
+import no.uka.findmyapp.android.rest.library.data.model.TemperatureMetaData;
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+public class TemperatureProvider extends ContentProvider{
+	private TemperatureDbHelper dbHelper; 
+	private static final int TEMPERATURE = 1; 
+	
+	private static final UriMatcher uriMatcher; 
+	
+	private static final HashMap<String, String> temperatureProjectionMap; 
+	static {
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(TemperatureMetaData.AUTHORITY, TemperatureMetaData.TemperatureTable.TABLE_NAME, TEMPERATURE);
+		
+		temperatureProjectionMap = new HashMap<String, String>(); 
+		temperatureProjectionMap.put(
+			TemperatureMetaData.TemperatureTable.ID, TemperatureMetaData.TemperatureTable.ID
+		);
+		temperatureProjectionMap.put(
+			TemperatureMetaData.TemperatureTable.LOCATION_ID, TemperatureMetaData.TemperatureTable.LOCATION_ID
+		);
+		temperatureProjectionMap.put(
+			TemperatureMetaData.TemperatureTable.VALUE, TemperatureMetaData.TemperatureTable.VALUE
+		);
+		temperatureProjectionMap.put(
+			TemperatureMetaData.TemperatureTable.DATE, TemperatureMetaData.TemperatureTable.DATE
+		);
+	}
+	
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		SQLiteDatabase db = dbHelper.getWritableDatabase(); 
+		int count = 0; 
+		switch (uriMatcher.match(uri)) {
+		case TEMPERATURE:
+			count = db.delete(TemperatureMetaData.TemperatureTable.TABLE_NAME, selection, selectionArgs);
+			break;
+
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri); 
+		}
+		
+		getContext().getContentResolver().notifyChange(uri, null);
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		switch (uriMatcher.match(uri)) {
+		case TEMPERATURE:
+			return TemperatureMetaData.CONTENT_TYPE_TEMPERATURE_ITEM;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues initValues) {
+		if(uriMatcher.match(uri) != TEMPERATURE) {
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+		
+		ContentValues values; 
+		if(initValues != null) {
+			values = new ContentValues(initValues); 
+		}
+		else {
+			values = new ContentValues(); 
+		}
+		
+		SQLiteDatabase db = dbHelper.getWritableDatabase(); 
+		
+		/* The second insert() parameter is a nullColumnHack, 
+		 * somewhat a crappy solution that is used to avoid 
+		 * that queries like "INSERT INTO tablename;" isn't 
+		 * declared illegal. Instead it automatically creates
+		 * a statement like "INSERT INTO temperature_table
+		 * (location_id) VALUES (NULL)" in this case.
+		 */
+		long rowId = db.insert(TemperatureMetaData.TemperatureTable.TABLE_NAME, TemperatureMetaData.TemperatureTable.LOCATION_ID, values);
+		if(rowId > 0) {
+			Uri temperatureUri = ContentUris.withAppendedId(TemperatureMetaData.CONTENT_PROVIDER_URI, rowId);
+			return temperatureUri;
+		}
+		throw new IllegalArgumentException("InsertUnknown URI: " + uri);
+	}
+
+	@Override
+	public boolean onCreate() {
+		this.dbHelper = new TemperatureDbHelper(getContext());
+		return false;
+	}
+	
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        switch (uriMatcher.match(uri)) {
+            case TEMPERATURE:
+                qb.setTables(TemperatureMetaData.TemperatureTable.TABLE_NAME);
+                qb.setProjectionMap(temperatureProjectionMap);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int count;
+        switch (uriMatcher.match(uri)) {
+            case TEMPERATURE:
+                count = db.update(TemperatureMetaData.TemperatureTable.TABLE_NAME, values, where, whereArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+}
---
+++ src/no/uka/findmyapp/android/rest/library/data/model/TemperatureMetaData.java
@@ -0,0 +1,32 @@
+package no.uka.findmyapp.android.rest.library.data.model;
+
+import java.sql.Date;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class TemperatureMetaData 
+{
+	private TemperatureMetaData() {}
+
+	public static final String AUTHORITY = "no.uka.findmyapp.android.rest.library.data.contentproviders.TemperatureProvider"; 
+	public static final Uri CONTENT_PROVIDER_URI = Uri.parse("content://" + AUTHORITY + "temperatures");
+	public static final String DATABASE_NAME = "temperature.db";
+	public static final int DATABASE_VERSION = 1; 
+
+	//TODO implement article MIME
+	//content type for one item: vnd.android.cursor.item/vnd.your-domain.your-item-name
+	public static final String CONTENT_TYPE_TEMPERATURE_ITEM = "vnd.android.cursor.item/vnd.uka.temperature"; 
+	
+	public class TemperatureTable implements BaseColumns {
+		private TemperatureTable() {}
+		
+		// Table name
+		public static final String TABLE_NAME = "temperature_table"; 
+		
+		// Databasefields
+		public static final String ID = "_id";
+		public static final String LOCATION_ID = "location_id"; 
+		public static final String VALUE = "value";
+		public static final String DATE = "date";
+	}
+}
